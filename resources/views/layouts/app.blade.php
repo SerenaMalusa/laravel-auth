1064a3f8ec66117ac51d9e4b41473b5d40da0605
SerenaMalusa <sere.malu@gmail.com>
2024-04-02 17:48:28 +0200

    feat: adjust page title and icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,26 @@
 <html lang="en">
 
   <head>
+    
+    <!-- carachters and viewport tags -->
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <meta content="ie=edge" http-equiv="X-UA-Compatible">
     <meta content="{{ csrf_token() }}" name="csrf-token">
-    <title>{{ env('APP_NAME', 'Laravel project') }} - @yield('title', 'My page') </title>
+
+    <!-- SEO tags -->
+    {{-- <meta name="author" content="{{ env('APP_AUTHOR') }}" /> --}}
+    <meta name="author" content="Serena Malusà" />
+    <meta name="description" content="laravel exercise">
+
+    <!-- title and icon -->
+    <title>{{ env('APP_NAME') }} @yield('title')</title>
+    <link rel="icon" href="{{ url('/logo-small.ico') }}">
+
+    <!-- css -->
+    @vite('resources/js/app.js')
+    
 
     @vite('resources/js/app.js')
 
